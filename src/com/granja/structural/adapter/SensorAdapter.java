87f3783b666769c527f10ae9ea0a304a44701006
MJMV25 <mjmunoz_108@cue.edu.co>
2025-10-27 17:57:20 -0500

    implementacion de patrones estructurales: Adapter, Decarator y Facade

--- src/com/granja/structural/adapter/SensorAdapter.java
+++ src/com/granja/structural/adapter/SensorAdapter.java
@@ -1,4 +1,15 @@
 package com.granja.structural.adapter;
 
 public class SensorAdapter {
+    private LegacySensor sensorAntiguo;
+
+    public SensorAdapter(LegacySensor sensorAntiguo) {
+        this.sensorAntiguo = sensorAntiguo;
+    }
+
+    public double leerTemperatura() {
+        double temp = sensorAntiguo.obtenerTemperaturaLegacy();
+        System.out.println("Adaptando lectura de sensor legacy: " + temp + "°C");
+        return temp;
+    }
 }
--- src/com/granja/structural/decorator/VacunaDecorator.java
+++ src/com/granja/structural/decorator/VacunaDecorator.java
@@ -1,4 +1,19 @@
 package com.granja.structural.decorator;
 
-public class VacunaDecorator {
+import com.granja.model.Animal;
+
+public class VacunaDecorator extends AnimalDecorator {
+    public VacunaDecorator(Animal animalDecorado) {
+        super(animalDecorado);
+    }
+
+    @Override
+    public void emitirSonido() {
+        super.emitirSonido();
+        aplicarVacuna();
+    }
+
+    private void aplicarVacuna() {
+        System.out.println("💉 Vacuna aplicada correctamente.");
+    }
 }
