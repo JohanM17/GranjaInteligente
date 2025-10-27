45420113b18ccd9defd67de841d21eab4a3e842c
Brandon0304 <bortiz_194@cue.edu.co>
2025-10-27 16:49:26 -0500

    implementacion de patrones Builder y Singleton

--- src/com/granja/creational/singleton/AlimentadorGlobal.java
+++ src/com/granja/creational/singleton/AlimentadorGlobal.java
@@ -1,4 +1,20 @@
 package com.granja.creational.singleton;
 
 public class AlimentadorGlobal {
+    private static AlimentadorGlobal instancia;
+
+    private AlimentadorGlobal() {
+        System.out.println("🐮 Alimentador global inicializado...");
+    }
+
+    public static AlimentadorGlobal getInstancia() {
+        if (instancia == null) {
+            instancia = new AlimentadorGlobal();
+        }
+        return instancia;
+    }
+
+    public void dispensarComida() {
+        System.out.println("🍽️ Dispensando alimento a todos los animales...");
+    }
 }
